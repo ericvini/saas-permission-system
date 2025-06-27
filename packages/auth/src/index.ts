2515bf60a42ed48e3b27cc3b9d56a744c6911165
ericvini <ericcobain3@gmail.com>
2025-06-27 18:27:52 -0300

    feat: setup com fastify e prisma

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,22 +1,42 @@
-import { AbilityBuilder, createMongoAbility } from '@casl/ability'
-import type { ForcedSubject, CreateAbility, MongoAbility } from '@casl/ability'
+import { z } from 'zod'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+import { AbilityBuilder, createMongoAbility } from '@casl/ability'
+import type { CreateAbility, MongoAbility } from '@casl/ability'
+import { permissions } from './permissions'
+import type { User } from './models/user'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
+
+const AppAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof AppAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+  const ability = builder.build({
+    detectSubjectType(subject) {
+      return subject.__typename
+    },
+  })
+
+  return ability
+}
