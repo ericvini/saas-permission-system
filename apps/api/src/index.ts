90bd3a08e8ccb26c61fb5a43ae046b79f65a7514
ericvini <ericcobain3@gmail.com>
2025-06-27 10:28:04 -0300

    feat: pacote de auth e primeiras permissões

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { ability } from '@saas/auth'
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOthersUsers = ability.can('delete', 'User')
+
+const userCannotDeleteOthersUsers = ability.cannot('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOthersUsers)
+console.log(userCannotDeleteOthersUsers)
