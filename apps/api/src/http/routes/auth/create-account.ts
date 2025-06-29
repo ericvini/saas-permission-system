f208ae0924e25ef4a0c472b1b8b57218cc6ed4ab
ericvini <ericcobain3@gmail.com>
2025-06-29 02:57:23 -0300

    feat: rota de cadastro de usuário e seed do BD

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,12 +1,15 @@
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
+        tags: ['Auth'],
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -14,8 +17,46 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created'
+    async (request, reply) => {
+      const { name, email, password } = request.body as {
+        name: string
+        email: string
+        password: string
+      }
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(409).send({ message: 'Email already in use' })
+      }
+
+      const autoJoinedOrganization = await prisma.organization.findFirst({
+        where: {
+          shouldAttachUsersByDomain: true,
+          domain: email.split('@')[1],
+        },
+      })
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+          member_on: autoJoinedOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinedOrganization.id,
+                },
+              }
+            : undefined,
+        },
+      })
+
+      return reply.status(201).send()
     }
   )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,16 +1,36 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+
 import { createAccount } from './routes/auth/create-account'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'next-saas-permission-system API',
+      description: 'API documentation for the next-saas-permission-system',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
